7512116e34d860c04fb680b8526747958d44df3b
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-02-15 16:11:00 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+fun main() {
+    var position = "akirachix"
+    println(position[0])
+    println(position[2])
+    println(position[3])
+    println(sentence("Jecinta",20))
+    println(length("Kenya"))
+    name("Terry")
+
+}
+ fun sentence(name: String,age: Int): String {
+     return "Hi, my name is $name and Iam $age years old"
+
+ }
+ fun length(name: String):String {
+     return length("country")
+ }
+fun name(name: String) {
+    var name= "Terry"
+    if (name == name){
+        println("That's me")
+    }
+    else{
+    println("I don't know who that is")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
